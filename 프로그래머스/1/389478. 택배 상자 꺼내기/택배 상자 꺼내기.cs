3deb46b858029d96e00d9ce6335e60b819ef746f
ChaeYoung You <ycy0109@naver.com>
2025-08-21 23:30:59 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.25 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
@@ -0,0 +1,50 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n, int w, int num)
+    {
+        int answer = 1;
+        int maxLine = 0;
+        int numLine = 0;
+        
+        int maxIndex = 0;
+        int numIndex = 0;
+        
+        
+        
+        maxLine = n % w == 0 ?  n / w : n / w + 1;
+        
+        numLine = num % w == 0 ? num / w : num / w + 1;
+        
+        maxIndex = maxLine % 2 == 0 ?  (w - 1) - (n - 1) % w : (n - 1) % w;
+        
+        numIndex = numLine % 2 == 0 ?  (w - 1) - (num - 1) % w : (num - 1) % w;
+        
+
+    
+            for(; numLine < maxLine; numLine++)
+            {
+                if(numLine % 2 == 0)
+                {
+                    if((numLine + 1) * w - (w - numIndex) <= n)
+                    {
+                        answer++;
+                    }
+                }
+                else
+                {
+                    if((numLine + 1) * w - numIndex <= n)
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+
+            
+        
+        
+        return answer;
+    }
+}
